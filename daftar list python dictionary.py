8fedfd0deb42072edbab5b771120b8875e4dd870
gustusbs <gustussitanggang@gmail.com>
2022-01-29 00:35:50 +0700

    list python dictionary

---
+++ daftar list python dictionary.py
@@ -0,0 +1,38 @@
+users = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+      "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+      }
+  }
+}
+print(users)
+print(users ["id"])
+print(users ["name"])
+print(users ["username"])
+print(users ["email"])
+print(users ["address"]["street"])
+print(users ["address"]["geo"]["lat"])
+print(users ["address"]["geo"]["lng"])
+
+print('\ntype dictionary (python)')
+print(users)
+print(type(users))
+
+print('\nubah dictionary (python) ke json (general)')
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+print('\nubah jscon (general) ke dictionary (python)')
+with open('result.json', 'w') as file:
+  json.dump(users, file)
